b9e360e1d3894943b62ee282fb2372c8f41071d5
Ian Meyer <ianmeyer@mac.com>
2015-06-19 09:29:36 -0400

    Initial Commit

---
+++ Summer2k15/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Summer2k15
+//
+//  Created by Ian Meyer on 6/19/15.
+//  Copyright (c) 2015 Ian Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
